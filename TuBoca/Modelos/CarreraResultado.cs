9a6ea45df749c38d55b422baf4bbdc92d4c270ce
Martín Mateos <mmateos@eest1chivilcoy.edu.ar>
2021-04-20 02:06:44 -0300

    Cree el modelo de las carreras que se muestran al final del resultado (carreras afines) y su archivo JSON. Este cuenta con las siguiente propiedades: ID (int), Nombre (string) y los tipos de personalidad (RIASEC) de tipo bool.

---
+++ TuBoca/Modelos/CarreraResultado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuBoca.Modelos
+{
+    public class CarreraResultado
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public bool Realista { get; set; }
+        public bool Investigador { get; set; }
+        public bool Artista { get; set; }
+        public bool Social { get; set; }
+        public bool Emprendedor { get; set; }
+        public bool Convencional { get; set; }
+    }
+}
